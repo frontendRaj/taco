25a532c442500496524050fe2f985b76f143d3da
rajdesigner025 <rajdesigner025@gmail.com>
2021-10-19 22:30:53 +0530

    add files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4 mt-4"> Button</button>
       </header>
+      <div class="w-full md:w-3/4 lg:w-1/2"></div>
+      <div class="border border-red-100 text-lg bg-red-400 text-white"></div>
     </div>
   );
 }
